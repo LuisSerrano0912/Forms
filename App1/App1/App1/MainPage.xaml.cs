742a687463fc0d66493fb6d1d4a20dd4966685bf
Luis Serrano <lserrano467@gmail.com>
2018-10-09 23:11:02 -0600

    Basic App Forms

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        List<genericObj> data = new List<genericObj>();
+        public MainPage()
+        {
+            InitializeComponent();
+            BtnLogin.Clicked += BtnLogin_Clicked;
+            BtnRegister.Clicked += BtnRegister_Clicked;
+        }
+
+        private void BtnRegister_Clicked(object sender, EventArgs e)
+        {
+            genericObj obj = new genericObj()
+            {
+                name = UserName.Text,
+                password = UserPass.Text
+            };
+            data.Add(obj);
+            UserName.Text = string.Empty;
+            UserPass.Text = string.Empty;
+            lblResult.Text = "Usuario Registado";
+            lblResult.TextColor = Color.Green;
+            Thread.Sleep(1000);
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            if(data.Capacity > 0)
+            {
+                genericObj obj = new genericObj() { name = UserName.Text, password = UserPass.Text };
+                var res = data.Find(c => c.name.Equals(UserName.Text) && c.password.Equals(UserPass.Text));
+
+
+                if (res != null)
+                {
+                    lblResult.Text = "Bienvenido " + UserName.Text;
+                    lblResult.TextColor = Color.Green;
+                    UserName.Text = string.Empty;
+                    UserPass.Text = string.Empty;
+                }
+                else
+                {
+                    lblResult.Text = "Nombre o contraseña incorrectos";
+                    lblResult.TextColor = Color.Red;
+                    UserName.Text = string.Empty;
+                    UserPass.Text = string.Empty;
+                }
+            }
+            else
+            {
+                lblResult.Text = "Debes registrar al menos 1 usuario...";
+                lblResult.TextColor = Color.Orange;
+                UserName.Text = string.Empty;
+                UserPass.Text = string.Empty;
+            }
+        }
+    }
+}
---
+++ App1/App1/App1/genericObj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    class genericObj
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+}
